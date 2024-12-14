b97fda30779d1f3e8087d47736c4eba04f2ee993
Dao Thai Ha <daothaiha1692005@gmail.com>
2024-12-14 02:30:26 +0200

    Add project files.

---
+++ Candidates/Candidate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace part_1
+{
+    public class Candidate
+    {
+        public int numOfVotes;
+        // Declare properties
+        public  string Firstname { get; set; }
+        public  string Lastname {  get; set; }
+        public  string Party { get; set; }
+
+        public int Votes
+        { 
+          get {  return numOfVotes; }
+          set
+          {
+                if (value >= 0) {
+                    numOfVotes = value;
+                }
+                else
+                {
+                    numOfVotes = 0;
+                }
+          }
+        }
+        
+        //Initialize constructor method
+        public Candidate( string first, string last, string party, int votes)
+        {
+            Firstname = first;
+            Lastname = last ;
+            Party = party;
+            Votes = votes;
+        }
+        // Override ToString method
+        public override string ToString() {
+            return $"{Lastname}, {Firstname} ({Party}): {Votes}";
+        }
+    }
+}
